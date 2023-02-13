fe14dbd69febf9dd328e06039974da395709f83c
Gianluca Boiano <491117+M0Rf30@users.noreply.github.com>
2023-02-13 22:56:48 +0100

    chore: reduce cyclomatic complexity
    * chore: reduce cyclomatic complexity
    
    * chore: apk: don't require privileged operations if not needed

--- debian/debian.go
+++ debian/debian.go
@@ -239,7 +239,7 @@ func (d *Debian) dpkgDeb() (string, error) {
 	for _, arch := range d.PKGBUILD.Arch {
 		newPath = filepath.Join(d.PKGBUILD.Home,
 			fmt.Sprintf("%s_%s-%s%s_%s.deb",
-				d.PKGBUILD.PkgName, d.PKGBUILD.PkgVer, d.PKGBUILD.PkgRel, d.PKGBUILD.Release,
+				d.PKGBUILD.PkgName, d.PKGBUILD.PkgVer, d.PKGBUILD.PkgRel, d.PKGBUILD.CodeName,
 				arch))
 	}
 
@@ -271,8 +271,47 @@ func (d *Debian) Update() error {
 	return err
 }
 
+func (d *Debian) createDebResources() error {
+	err := d.createConfFiles()
+	if err != nil {
+		return err
+	}
+
+	err = d.createControl()
+	if err != nil {
+		return err
+	}
+
+	err = d.createMd5Sums()
+	if err != nil {
+		return err
+	}
+
+	err = d.createScripts()
+	if err != nil {
+		return err
+	}
+
+	err = d.createDebconfTemplate()
+	if err != nil {
+		return err
+	}
+
+	err = d.createDebconfConfig()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
 func (d *Debian) Build() ([]string, error) {
 	var err error
+
+	err = d.createDebResources()
+	if err != nil {
+		return nil, err
+	}
 
 	d.InstalledSize, err = utils.GetDirSize(d.PKGBUILD.PackageDir)
 
@@ -295,36 +334,6 @@ func (d *Debian) Build() ([]string, error) {
 	d.debDir = filepath.Join(d.PKGBUILD.PackageDir, "DEBIAN")
 	err = utils.ExistsMakeDir(d.debDir)
 
-	if err != nil {
-		return nil, err
-	}
-
-	err = d.createConfFiles()
-	if err != nil {
-		return nil, err
-	}
-
-	err = d.createControl()
-	if err != nil {
-		return nil, err
-	}
-
-	err = d.createMd5Sums()
-	if err != nil {
-		return nil, err
-	}
-
-	err = d.createScripts()
-	if err != nil {
-		return nil, err
-	}
-
-	err = d.createDebconfTemplate()
-	if err != nil {
-		return nil, err
-	}
-
-	err = d.createDebconfConfig()
 	if err != nil {
 		return nil, err
 	}
