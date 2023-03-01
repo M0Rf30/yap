b6cf984d2ecca2b754720b9ea92ee7c3f5f46ce1
M0Rf30 <morf3089@gmail.com>
2023-03-01 16:34:18 +0100

    fix: source: missing empty path for utils.Exec()

--- utils/strings.go
+++ utils/strings.go
@@ -1,10 +1,33 @@
 package utils
 
 import (
+	"crypto/rand"
+	"math/big"
 	"strings"
 
 	"mvdan.cc/sh/v3/syntax"
 )
+
+// GenerateRandomString returns a securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomString(n int) string {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+
+	ret := make([]byte, n)
+
+	for index := 0; index < n; index++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return ""
+		}
+
+		ret[index] = letters[num.Int64()]
+	}
+
+	return string(ret)
+}
 
 // Generates a string from a *syntax.Assign of an array declaration.
 func StringifyArray(node *syntax.Assign) []string {
