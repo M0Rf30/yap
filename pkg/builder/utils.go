de5f4350359c03ac37daf96e4f5c57caa04163f4
M0Rf30 <morf3089@gmail.com>
2023-10-31 17:12:55 +0100

    refactor: improve readibility and simplify a lot of code

--- pkg/builder/utils.go
+++ pkg/builder/utils.go
@@ -18,15 +18,9 @@ func RunScript(cmds string) error {
 	script, _ := syntax.NewParser().Parse(strings.NewReader(cmds), "")
 
 	runner, _ := interp.New(
-		interp.Env(expand.ListEnviron(os.Environ()...,
-		)),
+		interp.Env(expand.ListEnviron(os.Environ()...)),
 		interp.StdIO(nil, os.Stdout, os.Stdout),
 	)
 
-	err := runner.Run(context.TODO(), script)
-	if err != nil {
-		return err
-	}
-
-	return err
+	return runner.Run(context.TODO(), script)
 }
