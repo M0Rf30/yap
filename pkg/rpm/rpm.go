715b275465367bb5d9b74b310bc02f57e11c519f
Gianluca Boiano <morf3089@gmail.com>
2024-10-19 18:23:28 +0200

    feat: rpm: create packages with pure go

--- pkg/rpm/rpm.go
+++ pkg/rpm/rpm.go
@@ -1,16 +1,18 @@
 package rpm
 
 import (
-	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
+	"github.com/M0Rf30/yap/pkg/options"
 	"github.com/M0Rf30/yap/pkg/pkgbuild"
-	"github.com/M0Rf30/yap/pkg/set"
 	"github.com/M0Rf30/yap/pkg/utils"
-	"github.com/otiai10/copy"
+	"github.com/google/rpmpack"
 )
 
 // RPM represents a RPM package.
@@ -18,20 +20,71 @@ import (
 // It contains the directory path of the package and the PKGBUILD struct, which
 // contains the metadata and build instructions for the package.
 type RPM struct {
-	PKGBUILD     *pkgbuild.PKGBUILD
-	RPMDir       string
-	buildDir     string
-	buildRootDir string
-	rpmsDir      string
-	sourcesDir   string
-	specsDir     string
-	srpmsDir     string
+	PKGBUILD *pkgbuild.PKGBUILD
 }
 
-// BuildPackage initiates the package building process for the RPM instance.
-// It calls the internal rpmBuild method and returns any errors encountered during the process.
-func (r *RPM) BuildPackage(_ string) error {
-	return r.rpmBuild()
+// BuildPackage creates an RPM package based on the provided PKGBUILD information.
+func (r *RPM) BuildPackage(artifactsPath string) error {
+	pkgName := r.PKGBUILD.PkgName +
+		"-" +
+		r.PKGBUILD.PkgVer +
+		"-" +
+		r.PKGBUILD.PkgRel +
+		"." +
+		r.PKGBUILD.ArchComputed +
+		".rpm"
+
+	epoch, _ := strconv.ParseUint(r.PKGBUILD.Epoch, 10, 32)
+	if epoch == 0 {
+		epoch = uint64(rpmpack.NoEpoch)
+	}
+
+	copyright := strings.Join(r.PKGBUILD.Copyright, "; ")
+	copyright = strings.TrimSuffix(copyright, " ")
+	license := strings.Join(r.PKGBUILD.License, " ")
+
+	pkgFilePath := filepath.Join(artifactsPath, pkgName)
+	rpm, _ := rpmpack.NewRPM(rpmpack.RPMMetaData{
+		Name:        r.PKGBUILD.PkgName,
+		Summary:     r.PKGBUILD.PkgDesc,
+		Description: r.PKGBUILD.PkgDesc,
+		Epoch:       uint32(epoch),
+		Version:     r.PKGBUILD.PkgVer,
+		Release:     r.PKGBUILD.PkgRel,
+		Arch:        r.PKGBUILD.ArchComputed,
+		Vendor:      copyright,
+		URL:         r.PKGBUILD.URL,
+		Packager:    r.PKGBUILD.Maintainer,
+		Group:       r.PKGBUILD.Section,
+		Compressor:  "zstd",
+		Licence:     license,
+		Provides:    processDepends(r.PKGBUILD.Provides),
+		Requires:    processDepends(r.PKGBUILD.Depends),
+		Conflicts:   processDepends(r.PKGBUILD.Conflicts),
+		Recommends:  processDepends(r.PKGBUILD.OptDepends),
+		Suggests:    processDepends(r.PKGBUILD.OptDepends),
+		BuildTime:   time.Now(),
+	})
+
+	r.addScriptFiles(rpm)
+
+	if err := r.addFiles(rpm); err != nil {
+		return err
+	}
+
+	cleanFilePath := filepath.Clean(pkgFilePath)
+
+	rpmFile, err := os.Create(cleanFilePath)
+	if err != nil {
+		return err
+	}
+	defer rpmFile.Close()
+
+	if err := rpm.Write(rpmFile); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // PrepareFakeroot sets up the environment for building an RPM package in a fakeroot context.
@@ -39,44 +92,13 @@ func (r *RPM) BuildPackage(_ string) error {
 // cleans up the RPM directory, creates necessary directories, and gathers files.
 // It also processes package dependencies and creates the RPM spec file, returning
 // an error if any step fails.
-func (r *RPM) PrepareFakeroot(artifactsPath string) error {
+func (r *RPM) PrepareFakeroot(_ string) error {
 	r.getGroup()
 	r.getRelease()
 	r.PKGBUILD.ArchComputed = RPMArchs[r.PKGBUILD.ArchComputed]
 
-	r.PKGBUILD.PkgDest, _ = filepath.Abs(artifactsPath)
-
-	if err := utils.RemoveAll(r.RPMDir); err != nil {
-		return err
-	}
-
-	if err := r.makeDirs(); err != nil {
-		return err
-	}
-
-	if err := r.getFiles(); err != nil {
-		return err
-	}
-
-	buildRootPackageDir := fmt.Sprintf("%s/%s-%s-%s.%s",
-		r.buildRootDir,
-		r.PKGBUILD.PkgName,
-		r.PKGBUILD.PkgVer,
-		r.PKGBUILD.PkgRel,
-		r.PKGBUILD.ArchComputed)
-
-	if err := copy.Copy(r.PKGBUILD.PackageDir, buildRootPackageDir); err != nil {
-		return err
-	}
-
-	r.PKGBUILD.Depends = r.processDepends(r.PKGBUILD.Depends)
-	r.PKGBUILD.MakeDepends = r.processDepends(r.PKGBUILD.MakeDepends)
-	r.PKGBUILD.OptDepends = r.processDepends(r.PKGBUILD.OptDepends)
-
-	tmpl := r.PKGBUILD.RenderSpec(specFile)
-	if err := r.PKGBUILD.CreateSpec(filepath.Join(r.specsDir,
-		r.PKGBUILD.PkgName+".spec"), tmpl); err != nil {
-		return err
+	if r.PKGBUILD.StripEnabled {
+		return options.Strip(r.PKGBUILD.PackageDir)
 	}
 
 	return nil
@@ -111,6 +133,19 @@ func (r *RPM) Install(artifactsPath string) error {
 	return nil
 }
 
+// Prepare prepares the RPM instance by installing the required dependencies.
+//
+// makeDepends is a slice of strings representing the dependencies to be installed.
+// It returns an error if there is any issue during the installation process.
+func (r *RPM) Prepare(makeDepends []string) error {
+	args := []string{
+		"-y",
+		"install",
+	}
+
+	return r.PKGBUILD.GetDepends("dnf", args, makeDepends)
+}
+
 // PrepareEnvironment prepares the environment for the RPM struct.
 //
 // It takes a boolean parameter `golang` which indicates whether or not to set up the Go environment.
@@ -135,19 +170,6 @@ func (r *RPM) PrepareEnvironment(golang bool) error {
 	return nil
 }
 
-// Prepare prepares the RPM instance by installing the required dependencies.
-//
-// makeDepends is a slice of strings representing the dependencies to be installed.
-// It returns an error if there is any issue during the installation process.
-func (r *RPM) Prepare(makeDepends []string) error {
-	args := []string{
-		"-y",
-		"install",
-	}
-
-	return r.PKGBUILD.GetDepends("dnf", args, makeDepends)
-}
-
 // Update updates the RPM object.
 //
 // It takes no parameters.
@@ -156,34 +178,8 @@ func (r *RPM) Update() error {
 	return nil
 }
 
-// getFiles retrieves a list of files from the RPM struct.
-//
-// This function iterates over the list of backup paths in the PKGBUILD struct
-// and adds them to the "backup" set. It then uses filepath.WalkDir to walk
-// through the files in the PKGBUILD.PackageDir directory. For each file, it
-// checks if it is a directory or a regular file. If it is a directory, it
-// checks if it is an empty directory and adds it to the "files" list if it is.
-// If it is a regular file, it adds it to the "files" list. After collecting all
-// the file paths, it removes the parent directories of each file path from the
-// "paths" set and adds the remaining file paths to the "PKGBUILD.Files" list in
-// the RPM struct. If a file path is found in the "backup" set, it is marked as
-// a config file and its path is prefixed with "%config". Finally, it returns
-// nil, indicating that no error occurred.
-//
-// No parameters.
-// Returns an error if there was an issue retrieving the files.
-func (r *RPM) getFiles() error {
-	backup := set.NewSet()
-	paths := set.NewSet()
-
-	for _, path := range r.PKGBUILD.Backup {
-		if !strings.HasPrefix(path, "/") {
-			path = "/" + path
-		}
-
-		backup.Add(path)
-	}
-
+// addFiles adds files from the specified package directory to the RPM package.
+func (r *RPM) addFiles(rpm *rpmpack.RPM) error {
 	var files []string
 
 	err := filepath.WalkDir(r.PKGBUILD.PackageDir,
@@ -209,32 +205,34 @@ func (r *RPM) getFiles() error {
 	}
 
 	for _, filePath := range files {
-		if len(filePath) < 1 ||
-			strings.Contains(filePath, ".build-id") {
-			continue
-		}
-
-		paths.Remove(filepath.Dir(filePath))
-		paths.Add(strings.TrimPrefix(filePath, r.PKGBUILD.PackageDir))
-	}
-
-	for pathInf := range paths.Iter() {
-		if pathInf != "" {
-			if !strings.HasPrefix(pathInf, "/") {
-				pathInf = "/" + pathInf
-			}
-
-			if backup.Contains(pathInf) {
-				pathInf = `%config "` + pathInf + `"`
-			} else {
-				pathInf = `"` + pathInf + `"`
-			}
-
-			r.PKGBUILD.Files = append(r.PKGBUILD.Files, pathInf)
-		}
+		cleanFilePath := filepath.Clean(filePath)
+		body, _ := os.ReadFile(cleanFilePath)
+		rpm.AddFile(rpmpack.RPMFile{
+			Name: strings.TrimPrefix(filePath, r.PKGBUILD.PackageDir),
+			Body: body,
+		})
 	}
 
 	return nil
+}
+
+// addScriptFiles adds script files to the RPM package based on the PKGBUILD configuration.
+func (r *RPM) addScriptFiles(rpm *rpmpack.RPM) {
+	if r.PKGBUILD.PreInst != "" {
+		rpm.AddPretrans(r.PKGBUILD.PreInst)
+	}
+
+	if r.PKGBUILD.PreRm != "" {
+		rpm.AddPretrans(r.PKGBUILD.PreRm)
+	}
+
+	if r.PKGBUILD.PostInst != "" {
+		rpm.AddPretrans(r.PKGBUILD.PostInst)
+	}
+
+	if r.PKGBUILD.PostRm != "" {
+		rpm.AddPretrans(r.PKGBUILD.PostRm)
+	}
 }
 
 // getGroup updates the section of the RPM struct with the corresponding
@@ -258,91 +256,27 @@ func (r *RPM) getRelease() {
 	}
 }
 
-// makeDirs creates the necessary directories for the RPM struct.
-//
-// It does not take any parameters.
-// It returns an error if any directory creation fails.
-func (r *RPM) makeDirs() error {
-	r.RPMDir = filepath.Join(r.PKGBUILD.StartDir, "RPM")
-	r.buildDir = filepath.Join(r.RPMDir, "BUILD")
-	r.buildRootDir = filepath.Join(r.RPMDir, "BUILDROOT")
-	r.rpmsDir = filepath.Join(r.RPMDir, "RPMS")
-	r.sourcesDir = filepath.Join(r.RPMDir, "SOURCES")
-	r.specsDir = filepath.Join(r.RPMDir, "SPECS")
-	r.srpmsDir = filepath.Join(r.RPMDir, "SRPMS")
-
-	for _, path := range []string{
-		r.RPMDir,
-		r.buildDir,
-		r.buildRootDir,
-		r.rpmsDir,
-		r.sourcesDir,
-		r.specsDir,
-		r.srpmsDir,
-	} {
-		if err := utils.ExistsMakeDir(path); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// processDepends takes a slice of strings and processes each string in order to
-// modify it and return a new slice of strings for rpm syntax.
-//
-// It splits each string into three parts: name, operator, and version. If the
-// string is split successfully, it combines the three parts into a new format
-// and replaces the original string in the slice.
-//
-// Parameters:
-//   - depends: a slice of strings to be processed.
-//
-// Returns:
-//   - a new slice of strings with modified elements for rpm syntax.
-func (r *RPM) processDepends(depends []string) []string {
+// processDepends converts a slice of strings into a rpmpack.Relations object.
+// It attempts to set each string in the slice as a relation.
+// If any error occurs during the setting process, it returns nil.
+func processDepends(depends []string) rpmpack.Relations {
 	pattern := `(?m)(<|<=|>=|=|>|<)`
 	regex := regexp.MustCompile(pattern)
+	relations := make(rpmpack.Relations, 0)
 
 	for index, depend := range depends {
 		result := regex.Split(depend, -1)
-
 		if len(result) == 2 {
 			name := result[0]
 			operator := strings.Trim(depend, result[0]+result[1])
 			version := result[1]
 			depends[index] = name + " " + operator + " " + version
 		}
+
+		if err := relations.Set(depends[index]); err != nil {
+			return nil
+		}
 	}
 
-	return depends
-}
-
-// rpmBuild builds an RPM package using the RPM package manager.
-//
-// It executes the 'rpmbuild' command with the necessary options and arguments
-// to build the RPM package. The package is built using the specified
-// specifications file and the resulting package is stored in the RPM directory.
-//
-// Returns an error if the 'rpmbuild' command fails to execute or if there
-// are any errors during the package building process.
-func (r *RPM) rpmBuild() error {
-	args := []string{
-		"--define",
-		"_topdir " +
-			r.RPMDir,
-		"-bb",
-		r.PKGBUILD.PkgName +
-			".spec",
-		"--target",
-		r.PKGBUILD.ArchComputed,
-	}
-
-	if pkgbuild.Verbose {
-		args = append(args, "--verbose")
-	} else {
-		args = append(args, "--quiet")
-	}
-
-	return utils.Exec(false, r.specsDir, "rpmbuild", args...)
+	return relations
 }
