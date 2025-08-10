c9cd8a0d4a48cc71654148792d66d99da9be935c
M0Rf30 <morf3089@example.com>
2025-08-10 02:12:02 +0200

    feat(packer): add package manager configurations and utilities
    - Consolidate package manager configurations into centralized map
    - Add new file utilities and test utilities modules
    - Improve package manager initialization with validation

--- pkg/packer/packer.go
+++ pkg/packer/packer.go
@@ -34,6 +34,94 @@ type Packer interface {
 	Update() error
 }
 
+// PackageManagerConfigs holds all package manager configurations.
+var PackageManagerConfigs = map[string]*PackageManagerConfig{
+	"apk": {
+		Name:        "apk",
+		InstallCmd:  "apk",
+		InstallArgs: []string{"add", "--allow-untrusted"},
+		UpdateArgs:  []string{"update"},
+		ArchMap: map[string]string{
+			"x86_64":  "x86_64",
+			"i686":    "x86",
+			"aarch64": "aarch64",
+			"arm":     "armhf",
+			"armv6h":  "armhf",
+			"armv7h":  "armv7",
+		},
+		BuildEnvDeps: []string{"bash", "build-base", "fakeroot"},
+	},
+	"apt": {
+		Name:        "apt",
+		InstallCmd:  "apt-get",
+		InstallArgs: []string{"--allow-downgrades", "--assume-yes", "install"},
+		UpdateArgs:  []string{"update"},
+		ArchMap: map[string]string{
+			"x86_64":  "amd64",
+			"i686":    "i386",
+			"aarch64": "arm64",
+			"arm":     "armel",
+			"armv6h":  "armel",
+			"armv7h":  "armhf",
+		},
+		BuildEnvDeps: []string{"build-essential", "fakeroot"},
+	},
+	"pacman": {
+		Name:        "pacman",
+		InstallCmd:  "pacman",
+		InstallArgs: []string{"-U", "--noconfirm"},
+		UpdateArgs:  []string{"-Sy"},
+		ArchMap: map[string]string{
+			"x86_64":  "x86_64",
+			"i686":    "i686",
+			"aarch64": "aarch64",
+			"arm":     "arm",
+			"armv6h":  "armv6h",
+			"armv7h":  "armv7h",
+		},
+		BuildEnvDeps: []string{"base-devel", "fakeroot"},
+	},
+	"yum": {
+		Name:        "dnf",
+		InstallCmd:  "dnf",
+		InstallArgs: []string{"-y", "install"},
+		UpdateArgs:  []string{}, // RPM doesn't need explicit update
+		ArchMap: map[string]string{
+			"x86_64":  "x86_64",
+			"i686":    "i686",
+			"aarch64": "aarch64",
+			"arm":     "arm",
+			"armv6h":  "armv6hl",
+			"armv7h":  "armv7hl",
+		},
+		GroupMap: map[string]string{
+			"admin": "System Environment/Base",
+			"base":  "System Environment/Base",
+			"devel": "Development/Tools",
+			"libs":  "System Environment/Libraries",
+			"utils": "Applications/System",
+		},
+		BuildEnvDeps: []string{"rpm-build", "fakeroot"},
+	},
+	"zypper": {
+		Name:        "dnf",
+		InstallCmd:  "dnf",
+		InstallArgs: []string{"-y", "install"},
+		UpdateArgs:  []string{},
+		ArchMap: map[string]string{
+			"x86_64":  "x86_64",
+			"i686":    "i686",
+			"aarch64": "aarch64",
+		},
+		GroupMap: map[string]string{
+			"admin": "System Environment/Base",
+			"devel": "Development/Tools",
+			"libs":  "System Environment/Libraries",
+		},
+		BuildEnvDeps: []string{"rpm-build", "fakeroot"},
+	},
+}
+
 // GetPackageManager returns a Packer interface based on the given package build and distribution.
 //
 // pkgBuild: A pointer to a pkgbuild.PKGBUILD struct.
@@ -41,6 +129,14 @@ type Packer interface {
 // Returns a Packer interface.
 func GetPackageManager(pkgBuild *pkgbuild.PKGBUILD, distro string) Packer {
 	pkgManager := constants.DistroToPackageManager[distro]
+
+	// Get configuration for the package manager
+	config := PackageManagerConfigs[pkgManager]
+	if config == nil {
+		osutils.Logger.Fatal("unsupported package manager", osutils.Logger.Args("manager", pkgManager))
+		return nil
+	}
+
 	switch pkgManager {
 	case "apk":
 		return &abuild.Apk{
